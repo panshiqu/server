33ba84c37293de2ad3248d2c9166a95990e3f342
panshiqu <panshiqu@sina.com>
2024-07-28 16:18:29 +0800

    server main

---
+++ game_server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"net"
+	"os"
+
+	"github.com/panshiqu/golang/logger"
+	"github.com/panshiqu/golang/pb"
+	"github.com/panshiqu/golang/utils"
+	"github.com/panshiqu/server/game_server/frame"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":60001")
+	if err != nil {
+		log.Fatal(utils.Wrap(err))
+	}
+
+	logger.Init()
+
+	s := grpc.NewServer()
+
+	pb.RegisterNetworkServer(s, &frame.NetworkServer{})
+
+	go func() {
+		utils.WaitSignal(os.Interrupt)
+
+		frame.Stop()
+
+		s.GracefulStop()
+	}()
+
+	slog.Info("serve", slog.Any("err", s.Serve(l)))
+}
